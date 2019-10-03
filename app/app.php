854a94c1f9380d878c941acb6c91e1f3a42e1741
Ugur Gungezerler <ugur@peakode.com>
2019-10-03 12:32:58 +0300

    add controllers

--- app/app.php
+++ app/app.php
@@ -1,20 +1,49 @@
 <?php
 /**
  * Local variables
+ *
  * @var \Phalcon\Mvc\Micro $app
  */
 
-/**
- * Add your routes here
- */
-$app->get('/', function () {
-    echo $this['view']->render('index');
+use Phalcon\Mvc\Micro\Collection as MicroCollection;
+
+$di->setShared('app', function () use ($app) {
+    return $app;
 });
+
+
+$app->get(
+  '/', function () {
+    echo $this['view']->render('index');
+})->setName('/');
+
+//$app->get('/user', function () {
+//    return $this->response($this->auth);
+//})->setName('profile');
+
+//AuthController
+$auth = new MicroCollection();
+$auth->setHandler(new AuthController);
+$auth->setPrefix('/auth');
+$auth->post('/login', 'login', 'login');
+$auth->post('/register', 'register', 'register');
+$app->mount($auth);
+
+//UserController
+$auth = new MicroCollection();
+$auth->setHandler(new UserController);
+$auth->setPrefix('/user');
+$auth->get('/', 'profile', 'profile');
+$auth->patch('/update', 'update', '');
+$app->mount($auth);
+
 
 /**
  * Not found handler
  */
-$app->notFound(function () use($app) {
-    $app->response->setStatusCode(404, "Not Found")->sendHeaders();
-    echo $app['view']->render('404');
-});
+$app->notFound(
+  function () use ($app) {
+      $app->response->setStatusCode(404, "Not Found")->sendHeaders();
+      $app->response->setJsonContent(['message'=>'404 not found']);
+      $app->response->send();
+  });
