151eec2379a86134d20a12c693d2a7eba29b51ec
Ugur Gungezerler <ugur@peakode.com>
2019-10-05 21:20:20 +0300

    add phpunit

---
+++ tests/Test/UnitTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Phalcon\Test;
+
+use Phalcon\Http\Client\Request;
+
+/**
+ * Class UnitTest
+ */
+class UnitTest extends UnitTestCase
+{
+    public function testTestCase()
+    {
+
+        $this->assertEquals(
+          "works",
+          "works",
+          "This is OK"
+        );
+
+        $this->assertEquals(
+          "works",
+          "works1",
+          "This will fail"
+        );
+    }
+}
